cdba6244207911010a136596c5631aa353de163e
codesuzyworld <codessuzy@gmail.com>
2024-12-11 14:17:26 -0500

    change the factory seed settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Project;
 use App\Models\Event;
 use App\Models\Location;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,17 +19,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         User::truncate();
         Type::truncate();
         Project::truncate();
         Event::truncate();
+        Location::truncate();
 
-        User::factory()->count(2)->create();
-        Type::factory()->count(3)->create();
+        // Create specific users
+        User::create([
+            'first' => 'Admin',
+            'last' => 'User',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' => Str::random(10),
+        ]);
+
+        // Create additional random users
+        User::factory()->count(1)->create();
+
+        // Create specific types
+        $types = [
+            ['title' => 'Webinar'],
+            ['title' => 'Concert'],
+            ['title' => 'Lecture'],
+            ['title' => 'Sports'],
+        ];
+
+        foreach ($types as $type) {
+            Type::create($type);
+        }
+
+        // Create other data
         Project::factory()->count(4)->create();
         Event::factory()->count(4)->create();
         Location::factory()->count(4)->create();
-            
     }
 }
